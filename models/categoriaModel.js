0ab7944f74e1d6648b6f5fb1cf6e1ca30e05f960
Laura <lauramarques2405@gmail.com>
2026-09-10 12:35:01 -0300

    Atualizar o categoriaModel.js

---
+++ models/categoriaModel.js
@@ -0,0 +1,36 @@
+//model - bd
+const db = require("../config/firebase");
+const ref = db.ref("categorias");
+
+//listar
+async function listar() {
+    let registros = await ref.once("value");
+    let dados = registros.val();
+
+    if (!dados) return [];
+
+    return Object.keys(dados).map(id => ({
+        id,
+        nome: dados[id].nome
+    }));
+}
+
+//exportar
+module.exports = {
+    listar
+};
+
+//salvar
+async function salvar(categoria) {
+    let novoRegistro = ref.push();
+
+    await novoRegistro.set({
+        nome: categoria.nome
+    });
+}
+
+//exportar
+module.exports = {
+    listar,
+    salvar
+};
